d27691b904fe029693bea6d01c8d997da2c7299b
aranmanaikkollai-hue <aranmanaikkollai@gmail.com>
2026-05-08 21:56:27 +0530

    Refactor recognizeText method implementation

--- app/src/main/java/com/propdf/editor/data/repository/OcrRepositoryImpl.kt
+++ app/src/main/java/com/propdf/editor/data/repository/OcrRepositoryImpl.kt
@@ -12,11 +12,8 @@ import javax.inject.Singleton
 class OcrRepositoryImpl @Inject constructor(
     private val tesseractEngine: TesseractOcrEngine
 ) : OcrRepository {
-
-    override suspend fun recognizeText(bitmap: Bitmap): OcrResult =
-        tesseractEngine.recognize(bitmap)
-
+    override suspend fun recognizeText(bitmap: Bitmap): OcrResult = tesseractEngine.recognize(bitmap)
     override suspend fun recognizeTextFromPdf(uri: Uri, pageIndex: Int): OcrResult {
-        throw NotImplementedError("PDF page OCR not yet implemented")
+        throw NotImplementedError()
     }
 }
