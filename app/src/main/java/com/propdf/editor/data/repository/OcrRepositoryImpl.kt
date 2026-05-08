7d89a7e5633f79a38836afa625c492d9af0b9515
aranmanaikkollai-hue <aranmanaikkollai@gmail.com>
2026-05-08 19:53:42 +0530

    Implement OcrRepositoryImpl for text recognition

---
+++ app/src/main/java/com/propdf/editor/data/repository/OcrRepositoryImpl.kt
@@ -0,0 +1,23 @@
+package com.propdf.editor.data.repository
+
+import android.graphics.Bitmap
+import android.net.Uri
+import com.propdf.editor.data.ocr.TesseractOcrEngine
+import com.propdf.editor.domain.model.OcrResult
+import com.propdf.editor.domain.repository.OcrRepository
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class OcrRepositoryImpl @Inject constructor(
+    private val tesseractEngine: TesseractOcrEngine
+) : OcrRepository {
+
+    override suspend fun recognizeText(bitmap: Bitmap): OcrResult =
+        tesseractEngine.recognize(bitmap)
+
+    override suspend fun recognizeTextFromPdf(uri: Uri, pageIndex: Int): OcrResult {
+        // TODO: Render page first, then run OCR
+        throw NotImplementedError("PDF page OCR not yet implemented")
+    }
+}
