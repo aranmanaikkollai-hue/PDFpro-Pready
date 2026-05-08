3b118bf33fe20281eeebff81634460aa661cbf7e
aranmanaikkollai-hue <aranmanaikkollai@gmail.com>
2026-05-08 21:57:09 +0530

    Simplify BookmarkRepositoryImpl methods
    Refactor BookmarkRepositoryImpl methods to empty implementations.

--- app/src/main/java/com/propdf/editor/data/repository/BookmarkRepositoryImpl.kt
+++ app/src/main/java/com/propdf/editor/data/repository/BookmarkRepositoryImpl.kt
@@ -1,6 +1,5 @@
 package com.propdf.editor.data.repository
 
-import com.propdf.editor.data.local.entity.BookmarkEntity
 import com.propdf.editor.domain.model.Bookmark
 import com.propdf.editor.domain.repository.BookmarkRepository
 import kotlinx.coroutines.flow.Flow
@@ -10,23 +9,7 @@ import javax.inject.Singleton
 
 @Singleton
 class BookmarkRepositoryImpl @Inject constructor() : BookmarkRepository {
-
-    override suspend fun addBookmark(bookmark: Bookmark) {
-        val entity = BookmarkEntity(
-            documentUri = bookmark.documentUri,
-            pageIndex = bookmark.pageIndex,
-            label = bookmark.label,
-            createdAt = bookmark.createdAt
-        )
-        // TODO: Insert via DAO
-    }
-
-    override suspend fun removeBookmark(id: Long) {
-        // TODO: Delete via DAO
-    }
-
-    override fun getBookmarksForDocument(documentUri: String): Flow<List<Bookmark>> = flow {
-        // TODO: Query via DAO
-        emit(emptyList())
-    }
+    override suspend fun addBookmark(bookmark: Bookmark) {}
+    override suspend fun removeBookmark(id: Long) {}
+    override fun getBookmarksForDocument(documentUri: String): Flow<List<Bookmark>> = flow { emit(emptyList()) }
 }
