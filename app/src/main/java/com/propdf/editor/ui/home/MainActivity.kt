a6cc657d3c6d42552987e053e983de499d3e9a8c
aranmanaikkollai-hue <aranmanaikkollai@gmail.com>
2026-05-09 12:02:54 +0530

    Add newline at end of MainActivity.kt

--- app/src/main/java/com/propdf/editor/ui/home/MainActivity.kt
+++ app/src/main/java/com/propdf/editor/ui/home/MainActivity.kt
@@ -9,6 +9,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private val viewModel: HomeViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
